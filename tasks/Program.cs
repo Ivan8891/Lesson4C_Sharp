8feb27776acde93c9cb618991e38fe10fe0716ec
Ivan <crueldog50@gmail.com>
2023-06-01 11:37:01 +1000

    task1

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿int RaiseToPower(int a, int b) // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+{
+    int result = a;
+    for( int i = 1; i < b; i++ )
+    {
+        result *= a;
+    }
+    return result;
+}
